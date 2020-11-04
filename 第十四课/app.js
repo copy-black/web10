95dc6f60e99ca8bfd01f172a8200aab124711e0d
copy-black <1848892406@qq.com>
2020-11-04 12:37:20 +0800

    提交

---
+++ 第十四课/app.js
@@ -0,0 +1,10 @@
+function apple(){
+    //var  let 
+    var array =[12,15,235,64,89,100];
+    for(let index=0; index < array.length;index++){
+        const element =array[index];
+        console.log(element);
+    }
+}
+
+apple();
